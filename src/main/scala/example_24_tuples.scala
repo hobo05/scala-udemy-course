1ccd7575e8389ea58ab1fe0a5f3f3e6cb5035451
Timothy Cheng <tim.cheng09@gmail.com>
2017-10-14 17:53:05 +0100

    updates

---
+++ src/main/scala/example_24_tuples.scala
@@ -0,0 +1,20 @@
+object example_24_tuples extends App {
+  val area = getRectangleArea(5, 8)
+
+  def getRectangleArea(length: Double, breadth: Double): Double = {
+    length * breadth
+  }
+
+  println(s"area=$area")
+
+  val area2 = getRectangleArea(5, 8)
+  println(s"area2=$area2")
+
+  val perimeterOfSquare = 20.0
+
+  println((getRectangleArea _).tupled({
+    val sideOfSquare = perimeterOfSquare / 4; (sideOfSquare, sideOfSquare)
+  }))
+
+}
+
---
+++ src/main/scala/example_26.scala
@@ -0,0 +1,24 @@
+object example_26 extends App {
+
+  val radius = 10.0
+
+  def getCircleStats(r: Double, PI: Double = 3.14) = {
+    def getCircleArea(r: Double) = PI * r * r
+
+    def getCircleCircumference(r: Double) = 2 * PI * r
+
+    (getCircleArea(r), getCircleCircumference(r))
+  }
+
+
+  def getCircleStats2(PI: Double = 3.14, r: Double) = {
+    def getCircleArea(r: Double) = PI * r * r
+
+    def getCircleCircumference(r: Double) = 2 * PI * r
+
+    (getCircleArea(r), getCircleCircumference(r))
+  }
+
+  getCircleStats(r = 10.0)
+
+}
